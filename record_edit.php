bd82eeb676ee7072f0f239e0dfef1d3a9073fa13
“YukiYamada1031” <office.deborah.0217@gmail.com>
2026-08-11 01:06:19 +0900

    create page of top

--- record_edit.php
+++ record_edit.php
@@ -1,3 +1,17 @@
 <?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/record_edit.css">
+  <link rel="icon" href="img/top.png">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
--- top.php
+++ top.php
@@ -1,3 +1,36 @@
 <?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/top.css">
+  <link rel="icon" href="img/top.png">
+  <title>I-Skedul</title>
+</head>
+<body>
+  <div class='container'>
+    <div class='title'>
+      <h1>I-SKEDUL</h1>
+      <p class='sub-title'>
+        毎日の予定と実績を記録して<br>
+        自分をもっと成長させよう
+      </p>
+    </div>
+
+    <div class='mainvisual'>
+      <img src='img/top.png' alt='ここにイメージ画像が表示される。'>
+    </div>
+
+    <div class='button-login'>
+      <a href='login.php'>ログイン</a>
+    </div>
+    <div class='button-new'>
+    <a href='new.php'>新規登録</a>
+    </div>
+  </div>
+</body>
+</html>
